13610ca34cb50df2d926734a136a06e2a649ab63
wangjiang <wangjiang988@gmail.com>
2017-11-22 12:17:24 +0800

    init asgardcms

---
+++ Modules/Translation/Resources/lang/menu/nl/menu.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'title'  => 'Menu',
+    'titles' => [
+        'menu' => 'Menu beheer',
+        'create menu' => 'Creëer een menu',
+        'edit menu' => 'Wijzig een menu',
+        'create menu item' => 'Creëer een menu item',
+        'edit menu item' => 'Wijzig een menu item',
+    ],
+    'breadcrumb' => [
+        'menu' => 'Menu beheer',
+        'create menu' => 'Creëer een menu',
+        'edit menu' => 'Wijzig een menu',
+        'create menu item' => 'Creëer een menu item',
+        'edit menu item' => 'Wijzig een menu item',
+    ],
+    'button' => [
+        'create menu item' => 'Creëer een menu item',
+        'create menu' => 'Creëer een menu',
+    ],
+    'table' => [
+        'name' => 'Naam',
+        'title' => 'Titel',
+    ],
+    'form' => [
+        'title' => 'Titel',
+        'name' => 'Naam',
+        'status' => 'Online',
+        'uri' => 'URI',
+        'url' => 'URL',
+        'primary' => 'Primair menu (gebruikt voor front-end routing)',
+    ],
+    'navigation' => [
+        'back to index' => 'Ga terug naar het menu overzicht',
+    ],
+    'list resource' => 'Menu overzicht',
+    'create resource' => 'Menu creëren',
+    'edit resource' => 'Menu wijzigen',
+    'destroy resource' => 'Menu verwijderen',
+];
